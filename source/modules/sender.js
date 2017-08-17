9b1797992c286788328d49763f49ed27caaeb325
OlliSayn <olli.saynatmaki@apinf.io>
2017-08-17 15:34:26 +0300

    Added sender configuration file and removed the commented unneeded code.
    Signed-off-by: OlliSayn <olli.saynatmaki@apinf.io>

--- source/modules/sender.js
+++ source/modules/sender.js
@@ -2,6 +2,8 @@
 
 const request = require("request");
 
+var sender_conf = require("./sender_config");
+
 /**
  * Sender of transformed data in api
  * @class Sender
@@ -32,16 +34,8 @@ class Sender {
      * @memberOf Sender
      */
     send(data, callback) {
-      //const url = "https://apinf.io/rest/v1/apis",
-      //const url = "https://nightly.apinf.io/rest/v1/apis",
-      const url = "http://localhost:3000/rest/v1/apis",
+      const url = sender_conf.send_apis_url,
             json = {
-                // api: {
-                //     id: this._guid(),
-                //     name: data.title,
-                //     description: data.description,
-                //     api_endpoint_url: data.endpoint
-                // }
                 name: data.title,
                 description: data.description,
                 url: data.endpoint
